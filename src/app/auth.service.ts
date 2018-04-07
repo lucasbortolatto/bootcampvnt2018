2357629e3a7503da2d94412fd8d605c4946e0804
Luidson Lucas Bortolatto <luidson.bortolatto@venturus.org.br>
2018-04-07 17:36:43 -0300

    Auth  with google

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
-export class AuthService implements CanActivate{
+export class AuthService implements CanActivate {
 
   canActivate() {
     console.log('CanActivate was called');
     return true;
   }
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) { }
 
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+
+  }
 }
